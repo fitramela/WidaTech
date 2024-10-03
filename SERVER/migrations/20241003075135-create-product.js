48088393108daa225996c8cadbd9bab5f3b33014
Nama Anda <email@anda.com>
2024-10-03 15:07:00 +0700

    model n database

---
+++ SERVER/migrations/20241003075135-create-product.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      invoiceNumber: {
+        type: Sequelize.INTEGER
+      },
+      item: {
+        type: Sequelize.STRING
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      totalCOGS: {
+        type: Sequelize.DECIMAL
+      },
+      totalPrice: {
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
